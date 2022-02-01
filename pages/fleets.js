82d105c603978a9f3883e9bb8b251e57fd058027
unknown <messironaldoabraham@gmail.com>
2022-02-01 07:40:40 -0800

    the page

--- pages/fleets.js
+++ pages/fleets.js
@@ -48,10 +48,10 @@ export default function Fleets() {
         <div className="font-serif lg:bg-snow pb-5 h-screen">
             <Navbar />
             <div className=" sm:hidden flex lg:text-3xl flex-col items-center justify-center font-semibold text-brand-color mb-5">
-                Most popular cars hired in VIP FLEETS are listed below
+                Most popular cars hired in VIP FLEETS are listed below.
                 <div className="mt-5 font-normal  mb-5 text-2xl text-black">
                       Not seeing what you like?<br/>
-                      Visit our instagram page <a href="">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.
+                      <div className="mt-2">Visit our instagram page <a href="">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.</div>
                     </div>
                    
                     </div>
@@ -61,7 +61,7 @@ export default function Fleets() {
                     </div>
                     <div className="lg:hidden ml-5 mb-5 mr-5">
                       Not seeing what you like?<br/>
-                      Visit our instagram page <a href="https://www.instagram.com/vipfleets" className="text-brand-color">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.
+                      <div className="mt-2">Visit our instagram page <a href="https://www.instagram.com/vipfleets" className="text-brand-color">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.</div>
                     </div>
 
                 {LoadImage.map((data) => {
