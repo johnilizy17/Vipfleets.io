36ea20c2dd2ce6cb851861c9af465863723c9d38
unknown <messironaldoabraham@gmail.com>
2022-02-01 07:32:42 -0800

    the added image

--- pages/fleets.js
+++ pages/fleets.js
@@ -19,6 +19,9 @@ export default function Fleets() {
             "image": pardo,
         },
         {
+            "image": pardo2,
+        },
+        {
             "image": ford,
         },
         {
@@ -35,7 +38,10 @@ export default function Fleets() {
         },
         {
             "image": lx570,
-        }
+        },
+        {
+            "image": wagon,
+        },
     ]
 
     return (
@@ -45,17 +51,17 @@ export default function Fleets() {
                 Most popular cars hired in VIP FLEETS are listed below
                 <div className="mt-5 font-normal  mb-5 text-2xl text-black">
                       Not seeing what you like?<br/>
-                      Visit our instagram page <a href="">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members
+                      Visit our instagram page <a href="">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.
                     </div>
                    
                     </div>
             <div className="sm:flex sm:flex-col sm:z-10 sm:justify-center sm:items-center lg:grid lg:grid-cols-3 sm:pt-10 sm:bg-snow lg:grap-3 lg:pl-20 lg:pr-20">
                 <div className=" lg:hidden  pl-5 pr-5 flex justify-center font-semibold text-brand-color mb-2">
-                    Most popular cars hired in VIP FLEETS  are listed below:
+                    Most popular cars hired in VIP FLEETS  are listed below
                     </div>
                     <div className="lg:hidden ml-5 mb-5 mr-5">
                       Not seeing what you like?<br/>
-                      Visit our instagram page <a href="https://www.instagram.com/vipfleets" className="text-brand-color">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members
+                      Visit our instagram page <a href="https://www.instagram.com/vipfleets" className="text-brand-color">(VIP FLEETS)</a> or call any of our hire lines and speak to a member of our team members.
                     </div>
 
                 {LoadImage.map((data) => {
