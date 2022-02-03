11beb8579cd35538e1b7800410a618fbc4f5d372
unknown <messironaldoabraham@gmail.com>
2022-02-02 18:33:26 -0800

    the corrections

--- pages/special_offer.js
+++ pages/special_offer.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Header/Navbar';
 import News from '../components/home/News';
 import lagos from '../asset/lagos.jpg'
