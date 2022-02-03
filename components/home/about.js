637c3f0724da845287e4b151d5e26fd48bdb098a
unknown <messironaldoabraham@gmail.com>
2022-02-02 17:02:26 -0800

    the correction

--- components/home/about.js
+++ components/home/about.js
@@ -21,8 +21,8 @@ export default function Airport() {
                    </div>
                         <div className="mt-5 text-justify">
                             We have a large fleet of vehicles for you to select from, why not press on  <Link passHref={true} href="/fleets">
-                                <span className="text-justify text-brand-color font-bold cursor-pointer underline ml-2 mr-2 ">OUR FLEETS </span>
-                            </Link> to view and hire the perfect ride for you
+                                <span className="text-justify text-brand-color font-bold cursor-pointer underline ml-2 mr-2 w-8">OUR FLEETS </span>
+                            </Link> to view and hire the perfect ride for you.
                   </div>
                         <div className="mt-5 text-justify">
                             We understand you have choices; therefore, it is important to us that you make <span className="text-brand-color">VIP FLEETS </span> your preferred choice.
